127d4605599c5df24d407fa75fc7901f41396e23
Hanieh Moeinian <moeinian.5.hanieh@gmail.com>
2021-05-14 23:50:31 +0430

    third-phase commit

--- main.py
+++ main.py
@@ -1,151 +1,216 @@
-# import csv
-#
-#
-# def register():
-#     while True:
-#         username = input('please enter a username: ')
-#         flag = False
-#         with open('login_info.csv', 'r', newline='') as login_file:
-#             csv_reader = csv.DictReader(login_file)
-#             for row in csv_reader:
-#                 if username in row['username']:
-#                     flag = True
-#                     print("there is already someone with this username!")
-#                     break
-#         if flag == False:
-#             while True:
-#                 password = input('please enter a password with more than 4 characters: ')
-#                 if len(password) <= 4:
-#                     print('too short password!')
-#                 else:
-#                     break
-#
-#             with open('login_info.csv', 'a', newline='') as login_file:
-#                 csv_writer = csv.DictWriter(login_file, fieldnames=['username', 'password'])
-#                 csv_writer.writerow({'username': username, 'password': password})
-#                 print("congratulations! you registered successfully.")
-#                 break
-#
-#
-# def log_in():
-#     flag = False
-#     counter = 0
-#     username = input("please enter your username: ")
-#     with open('login_info.csv', 'r', newline='') as login_file:
-#         csv_reader = csv.reader(login_file)
-#         for row in csv_reader:
-#             if username == row[0]:
-#                 with open('locked_users.txt','r') as locked_file:
-#                     for line in locked_file.readlines():
-#                         if username == line.strip():
-#                             print('your account is locked!')
-#                             return
-#                 password = row[1]
-#                 flag = True
-#                 break
-#         if flag is False:
-#             print('there is not such a user!')
-#         else:
-#             while True:
-#                 entered_password = input('please enter your password: ')
-#                 if entered_password == password:
-#                     print('welcome')
-#                     return {'username': username, 'password': password}
-#                 elif counter == 2:
-#                     print('your account is locked!')
-#                     with open('locked_users.txt','a') as locked_file:
-#                         locked_file.write(f'{username}\n')
-#                     break
-#                 else:
-#                     print('wrong password!')
-#                     counter += 1
-#
-# while True:
-#     action = input('what do you want to do? 1-register 2-login : ')
-#     if action == '1':
-#         register()
-#     elif action == '2':
-#         log_in()
-#     else:
-#         print('wrong input')
-
-
-##################################
-##register implementation by teacher
-# import pandas as pd
-# import hashlib
-# import csv
-# class User:
-#     def __init__(self,username,password):
-#         self.username = username
-#         self.password = password
-#
-#     @staticmethod
-#     def register():
-#         file_path = 'login_info.csv'
-#         df_account = pd.read_csv(file_path)
-#         list_username = list(df_account['username'])
-#         while True:
-#             username = input('please enter username')
-#             if username in list_username:
-#                 print('username exists!')
-#             else:
-#                 break
-#         password = input('please enter password')
-#         hashed_password = hashlib.sha256(password.encode('utf8')).hexdigest()
-#         object_user = User(username, hashed_password)
-#         row_account = [[object_user.username, object_user.password]]
-#         with open(file_path, 'a', newline='') as csv_account:
-#             csv_writer = csv.writer(csv_account)
-#             # writing the data row
-#             csv_writer.writerows(row_account)
-
-
-##make a file named main.py
-from administrator import Administrator
+import csv
+import hashlib
 from student import Student
+from administrator import Administrator
 from user import User
-import pickle
+import logging
 
-# with open('shimi.txt','rb') as f:
-#     x=pickle.load(f)
-#     for l in x:
-#         print(l.name)
+my_logger = logging.getLogger('logger')
+file_handler = logging.FileHandler('file.log')
+file_handler.setLevel(logging.DEBUG)
+s_format = logging.Formatter('%(levelname)s-%(asctime)s-%(name)s-%(message)s')
+file_handler.setFormatter(s_format)
+my_logger.addHandler(file_handler)
+my_logger.setLevel(logging.DEBUG)
+
+try:
+    with open('user_info.csv', 'r') as f:
+        user_info = list(csv.reader(f))
+        del user_info[0]
+except OSError:
+    print("an error occurred while opening the file user_info.csv")
+    my_logger.error("an error occurred while opening the file user_info.csv")
+try:
+    with open('locked_users.txt', 'r') as f:
+        locked_users = f.readlines()
+except OSError:
+    print("an error occurred while opening the file")
+    my_logger.error("an error occurred while opening the file locked_users.txt")
+try:
+    with open('courses.csv', 'r') as f:
+        courses = list(csv.reader(f))
+        del courses[0]
+except OSError:
+    print("an error occurred while opening the file")
+    my_logger.error("an error occurred while opening the file courses.csv")
+
+
 while True:
     print("------------------------------------------")
-    selected_item = input("what do you want to do?\n1-log in\n2-sign up\n3-exit\nyour choice: ")
-    if selected_item == '1':
-        user = User.log_in()
-        if isinstance(user, Administrator):
-            while True:
-                task = input("what do you want to do:\n1-import lessons\n2-accept or reject "
-                             "a student's unit selection\n3-exit\nyour choice: ")
-                if task == '1':
-                    major = input("please enter major: ")
-                    no_lessons = int(input("how many lessons do you want to import? "))
-                    user.import_lessons(major, no_lessons)
-                elif task == '2':
-                    user.accept_reject_unit_selection()
-                else:
-                    break
-        if isinstance(user, Student):
-            while True:
-                task = input("what do you want to do:\n1-take lessons\n"
-                             "2-see selected lessons\n3-exit\nyour choice: ")
-                if task == '1':
-                    while True:
-                        x = user.take_lessons(user.major)
-                        if x is False:
-                            break
-                        else:
-                            contnue = input("do you want to choose another lesson?y/n")
-                            if contnue == 'n':
-                                break
-                elif task == '2':
-                    user.see_selected_lessons()
-                else:
-                    break
-    elif selected_item == '2':
-        User.register()
+    try:
+        selected_item = input("what do you want to do?\n1-log in\n2-sign up\n3-exit\nyour choice: ")
+        assert selected_item in ['1', '2', '3']
+    except AssertionError:
+        print("wrong input! you should enter one of the numbers 1, 2 or 3")
+        my_logger.error("wrong input in line 39 of main.py")
     else:
-        break
+        if selected_item == '1':
+            username = input("please enter your username: ")
+            if User.is_locked(locked_users, username):
+                continue
+            else:
+                for row in user_info:
+                    if row[0] == username:
+                        password, role, major = row[1], row[2], row[3]
+                        break
+                else:
+                    print("there is not such a user!")
+                    continue
+                user = User(username, password, role, major)
+                correct_log_in = user.log_in(locked_users)
+                if correct_log_in:
+                    my_logger.info(f"{user.username} entered the system successfully")
+                if not correct_log_in:
+                    my_logger.info(f"{user.username} could not enter the system")
+                    continue
+                else:
+                    if user.role == 'student':
+                        user = Student(user.username, user.hashed_password, user.role, user.major)
+                        while True:
+                            try:
+                                task = input("what do you want to do:\n1-take lessons\n"
+                                             "2-see selected lessons\n3-omit a lesson\n"
+                                             "4-exit\nyour choice: ")
+                                assert task in ['1', '2', '3', '4']
+                            except AssertionError:
+                                print("wrong input! you should enter one of the numbers 1, 2, 3 or 4")
+                                my_logger.error("wrong input in line 69 of main.py")
+                            else:
+                                if task == '1':
+                                    while True:
+                                        x = user.take_lessons(user_info, courses, my_logger)
+                                        if x is False:
+                                            break
+                                        else:
+                                            while True:
+                                                try:
+                                                    contnue = input("do you want to choose another course?y/n ")
+                                                    assert contnue in ['y', 'n']
+                                                except AssertionError:
+                                                    print("")
+                                                else:
+                                                    break
+
+                                            if contnue == 'n':
+                                                break
+                                elif task == '2':
+                                    print("----------------------------------------")
+                                    print("these are your lessons in this semester: ")
+                                    user.see_selected_courses(user_info)
+                                elif task == '3':
+                                    print("----------------------------------------")
+                                    user.see_selected_courses(user_info)
+                                    for usr in user_info:
+                                        if usr[0] == user.username:
+                                            if int(usr[4]) != 0:
+                                                try:
+                                                    course_no = int(input("which course do you want to omit? ")) - 1
+                                                except ValueError:
+                                                    print("you should enter a number")
+                                                    my_logger.error("wrong input in line 105 of main.py")
+                                                else:
+                                                    user.omit_course(user_info, course_no, courses)
+                                elif task == '4':
+                                    break
+
+                    else:
+                        user = Administrator(user.username, user.hashed_password, user.role, user.major)
+                        while True:
+                            try:
+                                task = input("what do you want to do:\n1-import lessons\n2-accept or reject "
+                                             "a student's unit selection\n3-exit\nyour choice: ")
+                                assert task in ['1', '2', '3']
+                            except AssertionError:
+                                print("you should enter one of the values 1, 2 or 3")
+                                my_logger.error("wrong input in line 118 of main.py")
+                            else:
+                                if task == '1':
+                                    while True:
+                                        try:
+                                            no_courses = int(input("how many lessons do you want to import? "))
+                                        except ValueError:
+                                            print("you should enter a number")
+                                            my_logger.error("wrong input in line 128 of main.py")
+                                        else:
+                                            break
+                                    for course in range(no_courses):
+                                        print("------------------------------------------")
+                                        name = input("enter course's name: ")
+                                        teacher = input("enter course's teacher: ")
+                                        major = input("enter course's major: ")
+                                        units = input("enter course's units: ")
+                                        capacity = input("enter course's capacity: ")
+                                        print("------------------------------------------")
+                                        user.import_lesson(courses, name, teacher, major, units, capacity)
+                                        my_logger.info(f"course {name} was added to the system")
+                                elif task == '2':
+                                    print("------------------------------------------")
+                                    print('students: ')
+                                    for usr in user_info:
+                                        if usr[2] == 'student':
+                                            print(f"{usr[0]}")
+                                    stu = input("which student's unit selection do you want to check?\n"
+                                                "enter a name:  ")
+                                    print("------------------------------------------")
+                                    user.accept_reject_unit_selection(user_info, stu, my_logger)
+                                elif task == '3':
+                                    break
+        elif selected_item == '2':
+            try:
+                student_or_administrator = input("are you a student or an administrator?\n"
+                                                 "1-student\n2-administrator\nyour choice: ")
+                assert student_or_administrator in ['1', '2']
+            except AssertionError:
+                print("you should enter one of the values 1 or 2")
+                my_logger.error("wrong input in line 157 of main.py")
+            else:
+                if student_or_administrator == '2':
+                    key = input("please enter the security key: ")
+                    if key != 'a1z9f4':
+                        print("wrong security key. you can not register as an administrator!")
+                        continue
+                print("------------------------------------------")
+                while True:
+                    username = input('please enter username: ')
+                    for user in user_info:
+                        if username == user[0]:
+                            print('there is a user with this username! please choose another username.')
+                    else:
+                        break
+                password = input('please enter password: ')
+                hashed_password = hashlib.sha256(password.encode('utf8')).hexdigest()
+                if student_or_administrator == '1':
+                    major = input("please enter your major: ")
+                else:
+                    major = None
+                role = {'1': 'student', '2': 'administrator'}
+                user = User(username, hashed_password, role[student_or_administrator], major)
+                user.register(user_info)
+        elif selected_item == '3':
+            try:
+                with open('user_info.csv', 'w', newline='') as f:
+                    csv_writer = csv.writer(f)
+                    csv_writer.writerow(['username','password','role','major','no_units','courses'])
+                    csv_writer.writerows(user_info)
+            except OSError:
+                print(f"an error occurred while working with the file")
+                my_logger.error("error while writing in file user_info.csv")
+            try:
+                with open('locked_users.txt', 'w') as f:
+                    f.writelines(locked_users)
+            except OSError:
+                print(f"an error occurred while working with the file")
+                my_logger.error("error while writing in file locked_users.txt")
+            try:
+                with open('courses.csv', 'w', newline='') as f:
+                    csv_writer = csv.writer(f)
+                    csv_writer.writerow(['name','teacher','major','units','capacity','active'])
+                    csv_writer.writerows(courses)
+                    break
+            except OSError:
+                print(f"an error occurred while working with the file")
+                my_logger.error("error while writing in file courses.csv")
+
+
+
+
--- student.py
+++ student.py
@@ -4,67 +4,107 @@ import csv
 
 
 class Student:
-
-    def __init__(self, username, major=None, no_units=0, lessons=""):
+    def __init__(self, username, major=None, no_units=0, lessons=None):
         self.username = username
         self.major = major
         self.no_units = no_units
         self.lessons = lessons
 
     def take_lessons(self, major):
-        with open(f"{major}.txt", 'rb') as my_file:
-            lessons = pickle.load(my_file)
-            print("------------------------------------------")
-            print("this is the list of lessons you are allowed to take:")
-            for i, lsson in enumerate(lessons):
-                print(f"{i+1}-name: {lsson.name}, teacher: {lsson.teacher},"
-                      f" units: {lsson.units}, capacity: {lsson.capacity}")
-        with open('students.csv','r',newline='') as my_file:
-            students = list(csv.reader(my_file))
-            for j, student in enumerate(students):
-                if student[0] == self.username:
-                    while True:
-                        if int(student[2])<20:
-                            print()
-                            lsn_no = int(input("which lesson do you want to choose? "))-1
-                            if f"{lessons[lsn_no].name}" not in student[3].strip().split(','):
+        try:
+            with open(f"{major}.txt", 'rb') as my_file:
+                lessons = pickle.load(my_file)
+                print("------------------------------------------")
+                print("this is the list of lessons you are allowed to take:")
+                for i, lsson in enumerate(lessons):
+                    print(f"{i+1}-name: {lsson.name}, teacher: {lsson.teacher},"
+                          f" units: {lsson.units}, capacity: {lsson.capacity}")
+        except OSError as err:
+            print(f"OS error: {err}")
+        try:
+            with open('students.csv','r',newline='') as my_file:
+                students = list(csv.reader(my_file))
+                for j, student in enumerate(students):
+                    if student[0] == self.username:
+                        while True:
+                            if int(student[2])<20:
+                                print()
+                                lsn_no = int(input("which lesson do you want to choose? "))-1
+                                if f"{lessons[lsn_no].name}" not in student[3].strip().split(','):
 
-                                if lessons[lsn_no].active == True:
-                                    if int(lessons[lsn_no].units) + int(student[2])<20:
-                                        students[j][3] = lessons[lsn_no].name + ',' +\
-                                                         students[j][3]
-                                        students[j][2] = int(lessons[lsn_no].units) +\
-                                                         int(student[2])
-                                        Lesson.update_status(major, lessons[lsn_no].name)
+                                    if lessons[lsn_no].active == True:
+                                        if int(lessons[lsn_no].units) + int(student[2])<20:
+                                            students[j][3] = lessons[lsn_no].name + ',' +\
+                                                             students[j][3]
+                                            students[j][2] = int(lessons[lsn_no].units) +\
+                                                             int(student[2])
+                                            Lesson.update_status(major, lessons[lsn_no].name)
+                                        else:
+                                            print("you can not take more than 20 units in"
+                                                  " a semester!")
+                                            return False
                                     else:
-                                        print("you can not take more than 20 units in"
-                                              " a semester!")
-                                        return False
+                                            print("this lesson's capacity is full!")
                                 else:
-                                        print("this lesson's capacity is full!")
+                                    print("you can not take a lesson two times in a semester!")
+                            if int(students[j][2])<10:
+                                print("you need to take at least 10 units")
+                                continue
                             else:
-                                print("you can not take a lesson two times in a semester!")
-                        if int(students[j][2])<10:
-                            print("you need to take at least 10 units")
-                            continue
-                        else:
-                            break
-        with open('students.csv', 'w', newline='') as my_file:
-            csv_writer = csv.writer(my_file)
-            csv_writer.writerows(students)
+                                break
+        except OSError as err:
+            print(f"OS error: {err}")
+        try:
+            with open('students.csv', 'w', newline='') as my_file:
+                csv_writer = csv.writer(my_file)
+                csv_writer.writerows(students)
+        except OSError as err:
+            print(f"OS error: {err}")
 
     def see_selected_lessons(self):
-        with open('students.csv', 'r') as f:
-            students = list(csv.reader(f))
-            for i in students:
-                if self.username == i[0]:
-                    print(f"these are the lessons you have taken:\n{i[3].strip()[:-1]}\n"
-                          f"number of units you have taken: {i[2]}")
+        try:
+            with open('students.csv', 'r') as f:
+                students = list(csv.reader(f))
+
+                for i in students:
+                    if self.username == i[0]:
+                        print(f"#########\nthese are the lessons you have taken:"
+                              f"\n{i[3].strip()[:-1]}\n"
+                              f"number of units you have taken: {i[2]}\n#########")
+        except OSError as err:
+            print(f"OS error: {err}")
+
+    def omit_lesson(self):
+        try:
+            with open('students.csv', 'r', newline='') as f:
+                students = list(csv.reader(f))
+                for student in students:
+                    if student[0] == self.username:
+                        list_lessons = student[3].strip()[:-1].split(',')
+                        for i, lssn in enumerate(list_lessons):
+                            print(f"{i + 1}-{lssn}")
+                        lesson_no = int(input("which lesson do you want to omit? ")) - 1
+                        with open(f'{self.major}.txt', 'rb') as f:
+                            lsns = pickle.load(f)
+                            for lesson in lsns:
+                                if lesson.name == list_lessons[lesson_no]:
+                                    print(lesson.name, lesson.units)
+                                    unit = int(lesson.units)
+                                    student[2] = int(student[2]) - unit
+                                    break
+                        list_lessons.remove(list_lessons[lesson_no])
+                        student[3] = ','.join(list_lessons)
+                        student[3] += ','
+        except OSError as err:
+            print(f"OS error: {err}")
+        except ValueError as err:
+            print(f"value error: {err}")
+        try:
+            with open('students.csv', 'w', newline='') as f:
+                csv_writer = csv.writer(f)
+                csv_writer.writerows(students)
+        except OSError as err:
+            print(f"OS error: {err}")
 
 
-
-
-
-if __name__ == "__main__":
-    x = Student('asdf')
 
